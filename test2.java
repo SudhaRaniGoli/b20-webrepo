a29aaa99f64427eb492c9dcd984791278ec4dec7
SudhaRaniGoli <135088746+SudhaRaniGoli@users.noreply.github.com>
2023-06-21 22:57:13 -0400

    21 Jun test2.java

---
+++ test2.java
@@ -0,0 +1,7 @@
+Streamline the way IT operates and enterprises grow with observability and AIOps.
+As more organizations have adopted DevOps practices to streamline their software development, 
+  increase developer productivity, and enhance continuous delivery workflows to deliver better software faster.
+
+Our whitepaper covers all you need to know to understand the basic principles of DevOps, 
+  the benefits DevOps practices bring, and how DevOps along with other technologies can help transform your organization and deliver better software faster.
+  Topics include:
